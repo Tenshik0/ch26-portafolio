a0212d475c503eb246eb57afce4f7d986b315561
Tenshik0 <tenshik0.ale@gmail.com>
2023-05-03 17:34:11 -0600

    Cambiar colores en Adivina numero
    Se esta cambiando el front end

--- assets/js/fecha.js
+++ assets/js/fecha.js
@@ -3,33 +3,16 @@ function calcularFecha(){
     let mes = document.getElementById("mes").value;
     let anio = document.getElementById("anio").value;
 
-    const meses = {
-        'enero': 1,
-        'febrero': 2,
-        'marzo': 3,
-        'abril': 4,
-        'mayo': 5,
-        'junio': 6,
-        'julio': 7,
-        'agosto': 8,
-        'septiembre': 9,
-        'octubre': 10,
-        'noviembre': 11,
-        'diciembre': 12
-      };
-
-    let mesNumero = meses[mes.toLowerCase()];
-
-    //console.log(dia,mesNumero, anio);
-    
+    let mesNumero = calcularMes(mes);
+    console.log("La fecha ingresada es: " + dia + mesNumero + anio);
 
     let fecha = new Date(anio, mesNumero-1, dia);
-    //console.log(fecha);
+    console.log("Fecha es: " + fecha);
 
-    let diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    let diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
 
     let diaSemana = diasSemana[fecha.getDay()];
-    //console.log(diaSemana);
+    console.log(diaSemana);
     
     const diasEnSemana = {
         'lunes': 1,
@@ -43,7 +26,7 @@ function calcularFecha(){
 
     let diaNumero = diasEnSemana[diaSemana.toLowerCase()];
 
-    //console.log(diaNumero);
+    console.log(diaNumero);
 
     switch (diaNumero){
         case 1:case 2:case 3:case 4:case 5:
@@ -52,6 +35,33 @@ function calcularFecha(){
         case 6: case 7:
             document.getElementById("fechaDia").innerHTML = "El dia fue " + diaSemana + " no se labora";
             break;
+        default:
+          document.getElementById("fechaDia").innerHTML = "Faltan datos";
     }
+
+}
+
+function calcularMes(mes){
+  const meses = {
+    'enero': 1,
+    'febrero': 2,
+    'marzo': 3,
+    'abril': 4,
+    'mayo': 5,
+    'junio': 6,
+    'julio': 7,
+    'agosto': 8,
+    'septiembre': 9,
+    'octubre': 10,
+    'noviembre': 11,
+    'diciembre': 12
+  };
+
+  let mesNumero = meses[mes.toLowerCase()];
+  return mesNumero;
+
+}
+
+function calcularDia(){
 
 }
