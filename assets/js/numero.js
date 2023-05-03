b08546d28c0fba46f9c0f88677172219df349ea5
Tenshik0 <tenshik0.ale@gmail.com>
2023-05-02 21:36:56 -0600

    Correcion numero
    Ya funciona con algoritmo binario

--- assets/js/numero.js
+++ assets/js/numero.js
@@ -1,12 +1,32 @@
 function numero(){
-    let n = Math.floor(Math.random() * 100);
+    let min = 1;
+    let max = 100;
+    let numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;// let n = Math.floor(Math.random() * 100);
     let mensaje1 = "Tu numero es ";
     let mensaje2 = ", excelente decision";
 
-    validar = confirm("Tu numero es : " + n + " ?");
+    validar = confirm("Tu numero es : " + numeroAleatorio + " ?");
 
-    if(validar)
+    while(validar !== true){
+        pregunta = confirm("Es mayor?");
+        if(pregunta)
+            min = numeroAleatorio + 1;
+        else
+            max = numeroAleatorio - 1;
+
+        
+        numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
+        validar = confirm("Tu numero es : " + numeroAleatorio + " ?");
+    }
+
+    document.getElementById("numeroGenerado").innerHTML = mensaje1 + numeroAleatorio + mensaje2;
+    /*if(validar)
         document.getElementById("numeroGenerado").innerHTML = mensaje1 + n + mensaje2;
     else
-        numero();
+        pregunta = confirm("Es mayor?");
+        if(pregunta)
+            numero();
+        else
+            numero();
+    */
 }
