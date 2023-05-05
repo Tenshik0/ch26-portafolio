3660cc8e4910a6b1b70f9e877e7eae0d2170e19f
Tenshik0 <tenshik0.ale@gmail.com>
2023-05-04 20:05:50 -0600

    Finalizado la actividad fecha
    Se finalizo, hay un detalle con el texto pero veré como hacerlo mas interactivo.

--- assets/js/fecha.js
+++ assets/js/fecha.js
@@ -2,43 +2,8 @@ function calcularFecha(){
     let dia = document.getElementById("dia").value;
     let mes = document.getElementById("mes").value;
     let anio = document.getElementById("anio").value;
-
-    let mesNumero = calcularMes(mes);
-    console.log("La fecha ingresada es: " + dia + mesNumero + anio);
-
-    let fecha = new Date(anio, mesNumero-1, dia);
-    console.log("Fecha es: " + fecha);
-
-    let diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
-
-    let diaSemana = diasSemana[fecha.getDay()];
-    console.log(diaSemana);
-    
-    const diasEnSemana = {
-        'lunes': 1,
-        'martes': 2,
-        'miercoles': 3,
-        'jueves': 4,
-        'viernes': 5,
-        'sabado': 6,
-        'domingo': 7
-      };
-
-    let diaNumero = diasEnSemana[diaSemana.toLowerCase()];
-
-    console.log(diaNumero);
-
-    switch (diaNumero){
-        case 1:case 2:case 3:case 4:case 5:
-            document.getElementById("fechaDia").innerHTML = "El dia fue " + diaSemana + " se labora";
-            break;
-        case 6: case 7:
-            document.getElementById("fechaDia").innerHTML = "El dia fue " + diaSemana + " no se labora";
-            break;
-        default:
-          document.getElementById("fechaDia").innerHTML = "Faltan datos";
-    }
-
+    //console.log("Los datos anotados fueron: " + dia + mes + anio);
+    validar(dia, mes, anio);
 }
 
 function calcularMes(mes){
@@ -57,11 +22,95 @@ function calcularMes(mes){
     'diciembre': 12
   };
 
-  let mesNumero = meses[mes.toLowerCase()];
-  return mesNumero;
+  return mesNumero = meses[mes.toLowerCase()];;
 
 }
 
-function calcularDia(){
+function obtenerFechaDeDate (anio, mesNumero, dia){
+  return fecha = new Date(anio, mesNumero-1, dia);
+}
 
+function obtenerDia (fecha){
+  let diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+  return diaSemana = diasSemana[fecha.getDay()];
+}
+
+function obtenerNumeroDia(diaSemana){
+  const diasEnSemana = {
+    'lunes': 1,
+    'martes': 2,
+    'miercoles': 3,
+    'jueves': 4,
+    'viernes': 5,
+    'sabado': 6,
+    'domingo': 7
+  };
+  return diaNumero = diasEnSemana[diaSemana.toLowerCase()];
+}
+
+function imprimirEnPantalla(diaNumero, diaSemana){
+  switch (diaNumero){
+    case 1:case 2:case 3:case 4:case 5:
+        document.getElementById("fechaDia").innerHTML = "El dia fué " + diaSemana + " se laboró";
+        break;
+    case 6: case 7:
+        document.getElementById("fechaDia").innerHTML = "El dia fué " + diaSemana + " no se laboró";
+        break;
+    default:
+      document.getElementById("fechaDia").innerHTML = "Faltan datos";
+}
+}
+
+function validar(dia, mes, anio){
+  if(dia === "" || anio === "")
+    document.getElementById("fechaDia").innerHTML = "Faltan datos, no se puede calcular el día ";
+  else if(dia > 31)
+    document.getElementById("fechaDia").innerHTML = "En un mes solo puede haber 31 días máximo, revisa el día ";
+  else if(dia < 0)
+    document.getElementById("fechaDia").innerHTML = "Los meses no tienen días negativos, revisa el día ";
+  else if(anio < 1000 || anio > 3000)
+    document.getElementById("fechaDia").innerHTML = "Este programa solo calcula del año 1000 al 3000, revisa el año ";
+  else if(mes === "Febrero")
+    anioBisiesto(dia, mes, anio);
+  else
+    procesoParaCalcularFecha(dia, mes, anio);
+}
+
+function procesoParaCalcularFecha(dia, mes, anio){
+  let mesNumero = calcularMes(mes);
+    //console.log("La fecha ingresada es: " + dia + mesNumero + anio);
+
+    let fecha = obtenerFechaDeDate(anio, mesNumero, dia);
+    //console.log("Fecha es: " + fecha);
+
+    let diaSemana = obtenerDia(fecha);
+    //console.log(diaSemana);
+    
+    let diaNumero = obtenerNumeroDia(diaSemana);
+    //console.log(diaNumero);
+
+    imprimirEnPantalla(diaNumero,diaSemana);
+}
+
+function anioBisiesto(dia, mes, anio){
+  if((anio % 4) != 0){
+    //console.log("No es bisiesto");
+    //console.log(dia);
+    if(dia == 29){
+      //console.log("No se puede");
+      document.getElementById("fechaDia").innerHTML = "El año " + anio + " no es bisiesto. Revisa tu día en el mes de Febrero";
+    }
+    else if (dia > 29){
+      //console.log("No se puede mas de 29 dias");
+      document.getElementById("fechaDia").innerHTML = "En el año " + anio + ", Febrero solo tiene 28 días (Año no bisiesto)";
+    }else{
+      procesoParaCalcularFecha(dia, mes, anio);
+    }
+  }else{
+    console.log("Es bisiesto");
+    if (dia > 29)
+      document.getElementById("fechaDia").innerHTML = "En el año " + anio + ", Febrero solo tiene 29 días ( Año bisiesto)";
+    else
+      procesoParaCalcularFecha(dia, mes, anio);
+  }
 }
